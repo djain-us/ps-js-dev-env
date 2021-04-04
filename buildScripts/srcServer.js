9d6496067b4740615d40a3ccdd7884254df2f61d
Dinesh Jain <dinesh.jain.us@gmail.com>
2021-04-04 05:38:23 -0400

    Initial commit

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,27 @@
+import express from "express";
+import path from "path";
+import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
+import webpack_dev_middleware from "webpack-dev-middleware"; 
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+const webpack_dm = require("webpack-dev-middleware")(compiler, {
+  publicPath: config.output.publicPath
+});
+
+app.use(webpack_dm);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Erro loading website.")
+  } else {
+    console.log(`Server is running on http://localhost:${port}`);
+  }
+});
